fccec1b243ba987489cd8ae00fc9fd6b6ca2b73e
Zarif <zarifraja19@gmail.com>
2023-10-08 18:47:45 +0600

    auth in route(home)

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,21 +1,25 @@
-import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Components/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import AuthProvider from "../AuthProvider/AuthProvider";
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-      children: [
-        {path: "/", element: <Home/>},
-        {path: "/login", element: <Login/>},
-      {path: "/register", element: <Register/>},
-
-      ]
-    },
-  ]);
-  export default router;
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <AuthProvider>
+            <Home />
+          </AuthProvider>
+        ),
+      },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ],
+  },
+]);
+export default router;
